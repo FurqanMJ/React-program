96277d2bf1db5ee9f5a2db5473ccf2dd69c12b2c
FurqanMJ <furkhanshaikh41@gmail.com>
2024-05-25 06:22:35 +0530

    change

--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
+// src/index.js
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import Hello from './App'; // Import the Hello component
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const container = document.getElementById('root'); // This should match the id in your public/index.html
+ReactDOM.createRoot(container).render(<Hello />);
